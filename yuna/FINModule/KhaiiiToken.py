c8644fe2948ebb8cdc2bc45f8ffd2bc9a8027f13
yuna bae <yuna11do@gmail.com>
2020-03-17 02:07:47 +0900

    khaiii_module_changed

--- yuna/FINModule/KhaiiiToken.py
+++ yuna/FINModule/KhaiiiToken.py
@@ -10,24 +10,24 @@ class khaiii_complexnoun:
 
     def khaiii_complex(self,filename = ''):
         df = self.df
+        def khaiii(sentence):
+            for word in self.api.analyze(str(sentence)):
+                for m in word.morphs:
+                    self.lex.append(m.lex)
+                    self.tag.append(m.tag)
 
-        for word in self.api.analyze(str(sentence)):
-            for m in word.morphs:
-            self.lex.append(m.lex)
-            self.tag.append(m.tag)
+            for i in range(len(self.tag)-1):
+                if self.tag[i][0] == 'N' and self.tag[i+1][0] == 'N':
+                    self.complex_nouns.append(self.lex[i] + ' ' + self.lex[i+1])
+                elif self.tag[i] in ['NNG', 'NNP', 'NNB', 'NR', 'NP', 'VA'] and len(self.lex[i])>1:
+                    self.complex_nouns.append(self.lex[i])
 
-        for i in range(len(self.tag)-1):
-            if self.tag[i][0] == 'N' and self.tag[i+1][0] == 'N':
-                self.complex_nouns.append(self.lex[i] + ' ' + self.lex[i+1])
-            elif self.tag[i] in ['NNG', 'NNP', 'NNB', 'NR', 'NP', 'VA'] and len(self.lex[i])>1:
-                self.complex_nouns.append(self.lex[i])
+            return complex_nouns
 
-        return complex_nouns  
+        df['ner_khaiii_token'] = df['ner_clean'].apply(khaiii)
 
-    df['ner_khaiii_token'] = df['ner_clean'].apply(khaiii_complex)
-
-    if filename !='':
-            df.to_csv(filename,index = False)
-    return df
+        if filename !='':
+                df.to_csv(filename,index = False)
+        return df
  
 
